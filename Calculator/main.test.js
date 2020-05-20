39ad350d8b2c025220190e5c3d7c0ea6befdac3b
matenemeth1994 <mate.nemeth@nology.io>
2020-05-20 12:40:25 +0100

    added calculator

---
+++ Calculator/main.test.js
@@ -0,0 +1,7 @@
+import { calculator, updateDisplay } from "./main";
+
+// describe('calculator', () => {
+// })
+        test('calculator object should exist', () => {
+            expect(typeof calculator).toEqual('Object')
+        });
