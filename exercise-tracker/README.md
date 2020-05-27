a76d812cd2dfa7cba0b1a7c17d1b8b5b11b58459
matenemeth1994 <mate.nemeth@nology.io>
2020-05-27 17:44:43 +0100

    finished backend

---
+++ exercise-tracker/README.md
@@ -0,0 +1,8 @@
+### Commands
+
+- create react app: `npx create-react-app mern-exercise-tracker`
+- \backend `npm init -y`
+- \backend `npm install express cors mongoose dotenv`. Cors = Cross Origin Resource Sharing. It provides middleware. Mongoose is interacting with mongodb through nodejs. dotenv = Instead of setting up environent variable in the system it creates environment variable in a file.
+- `npm install -g nodemon`
+- `npm install bootstrap`
+- `npm install react-router-dom`
---
+++ exercise-tracker/mern-exercise-tracker/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/navbar.component"
+import Exerciseslist from "./components/exercises-list.component"
+import EditExercise from "./components/edit-exercise.component"
+import CreateExercise from "./components/create-exercise.component"
+import CreateUser from "./components/create-user.component"
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <br />
+      <Route path="/" exact component={Exerciseslist} />
+      <Route path="/edit/:id" component={EditExercise} />
+      <Route path="/create" component={CreateExercise} />
+      <Route path="/user" component={CreateUser} />
+    </Router>
+  );
+}
+
+export default App;
