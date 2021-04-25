86a8f38b25eea0b25824d947c75b06490c2950c5
matenemeth1994 <mate.nemeth@nology.io>
2021-04-25 10:57:50 +0100

    added handleclick function to Game.js

--- tic-tac-to/src/Components/Game/Game.jsx
+++ tic-tac-to/src/Components/Game/Game.jsx
@@ -8,7 +8,14 @@ const Game = () => {
   const [xIsNext, setXisNext] = useState(true);
   const winner = calculateWinner(board);
 
-  const handleClick = () => {};
+  const handleClick = (i) => {
+    const boardCopy = [...board];
+    if (winner || boardCopy[i]) return;
+
+    boardCopy[i] = xIsNext ? 'X' : 'O';
+    setBoard(boardCopy);
+    setXisNext(!xIsNext);
+  };
 
   const jumpTo = () => {};
 
